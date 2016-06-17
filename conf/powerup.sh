9e92448040b9393b1888ad8b4c30a49045c14a8c
George Yanev <ge.yanev@gmail.com>
2016-06-17 07:02:36 +0300

    Improver vim and change Bash ANSI colors

--- conf/powerup.sh
+++ conf/powerup.sh
@@ -9,6 +9,15 @@ echo "PowerUP this account with neccessary development tools"
 script_path=$(readlink -f $0)
 cd $script_path
 
+echo "Bash"
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+cat >> ~/.bashrc <<'EOF'
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
+[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
+EOF
+
 echo "Vim"
 ln -s vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
