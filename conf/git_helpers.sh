8a363c16e4859d739f18ac2b71130152ba0bf99b
George Yanev <ge.yanev@gmail.com>
2016-06-17 07:47:01 +0300

    Ansible playbook (WIP) to install the necessary components
    * vim improvements

--- conf/git_helpers.sh
+++ conf/git_helpers.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+echo "Install Ansible"
+pip install ansible
+
+echo "Run Ansible"
+ansible-playbook -i ,127.0.0.1 os_powerup.yml
 echo "setup git PROMPT"
 cd ~
 git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
@@ -8,7 +14,6 @@ cat >> .bashrc <<'EOF'
 source ~/.bash-git-prompt/gitprompt.sh
 GIT_PROMPT_ONLY_IN_REPO=1
 EOF
-
 
 echo "git bash completion"
 curl http://git.io/vfhol > ~/.git-completion.bash && echo '[ -f ~/.git-completion.bash ] && . ~/.git-completion.bash' >> ~/.bashrc
